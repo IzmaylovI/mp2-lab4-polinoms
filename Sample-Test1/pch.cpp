cab2e58c842756337b4b80abc7b6020fe648858a
IzmaylovI <ilshat.izmaylov.03@mail.ru>
2022-04-27 02:17:54 +0300

    Laboratory work of Ilshat

--- Sample-Test1/pch.cpp
+++ Sample-Test1/pch.cpp
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 //
 // pch.cpp
 // Include the standard header and generate the precompiled header.
 //
 
 #include "pch.h"
-=======
-//
-// pch.cpp
-// Include the standard header and generate the precompiled header.
-//
-
-#include "pch.h"
->>>>>>> 0313d6a0bcb831d82a3e6eade187621deaf054e0
--- Sample-Test1/pch.h
+++ Sample-Test1/pch.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //
 // pch.h
 // Header for standard system include files.
@@ -8,15 +7,4 @@
 
 #include "gtest/gtest.h"
 #include "List.h"
-=======
-//
-// pch.h
-// Header for standard system include files.
-//
-
-#pragma once
-
-#include "gtest/gtest.h"
-#include "List.h"
->>>>>>> 0313d6a0bcb831d82a3e6eade187621deaf054e0
 #include "Polinomr.h"
